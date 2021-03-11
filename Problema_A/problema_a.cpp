f6ae5ec35fa482e89c70a53a8b57c9a6d686c071
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2021-03-11 18:34:30 +0000

    DONE <3 <3 <3

--- Problema_A/problema_a.cpp
+++ Problema_A/problema_a.cpp
@@ -10,12 +10,14 @@ int board_size, max_moves;
 class Board {
   public:
     vector<vector<int>> board;
-    vector<tuple<int, int>> vector_occ;
+    int occ, sum;
 
     void buildBoard(){
         string buffer;
         vector<int> line;
         int aux;
+
+        sum = 0, occ = 0;
 
         for (int i=0; i<board_size; i++){
             line.clear();
@@ -25,70 +27,13 @@ class Board {
                 line.insert(line.end(), aux);
 
                 if(aux != 0){
-                    buildVector(aux);
+                    occ++;
+                    sum += aux;
+                   // buildVector(aux);
                 }
 
             }
             board.insert(board.end(), line);
-        }
-    }
-
-    void buildVector(int num){
-        vector<tuple<int, int>>::iterator it;
-
-        if(vector_occ.empty()){
-            vector_occ.emplace_back(num, 1);
-        }
-        else{
-
-            for(it = vector_occ.begin(); it < vector_occ.end(); it++){
-
-                if(get<0>(*it) == num){
-                    get<1>(*it) ++;
-                    return;
-                }
-                else if(get<0>(*it) > num){
-                    vector_occ.insert(it, make_tuple(num, 1));
-                    return;
-                }
-            }
-
-            if(it == vector_occ.end()){
-                vector_occ.emplace_back(make_tuple(num, 1));
-            }
-        }
-    }
-
-    void updateVector(int num){ //numero que se fez merge
-        vector<tuple<int, int>>::iterator it;
-        int num2 = num*2;
-
-        for(it = vector_occ.begin(); it < vector_occ.end(); it++){
-
-            if(get<0>(*it) == num && get<1>(*it)>=2){
-                get<1>(*it) -= 2;
-                if(get<1>(*it) == 0){
-                    vector_occ.erase(it);
-                }
-                else{
-                    it++;
-                }
-                break;
-            }
-        }
-
-
-        if(it < vector_occ.end()){
-
-            if(get<0>(*it) == num2){
-                get<1>(*it) += 1;
-            }
-            else if(get<0>(*it) > num2){
-                vector_occ.insert(it, make_tuple(num2, 1));
-            }
-        }
-        else{
-            vector_occ.emplace_back(make_tuple(num2, 1));
         }
     }
 
@@ -101,13 +46,6 @@ class Board {
         cout << endl;
     }
 
-    void printVector(){
-        for (vector<tuple<int, int>>::iterator row= vector_occ.begin(); row < vector_occ.end(); row++) {
-            cout << get<0>(*row) << "," << get<1>(*row) << " ";
-        }
-        cout << endl;
-    }
-
     // MOVES
     //LEFT
     bool shiftLeft(int& move){
@@ -115,6 +53,7 @@ class Board {
       int col;
       int value, value_next;
       bool isUpdated = false;
+      bool shifted = false;
 
       for(i = 0; i< board_size; i++){
 
@@ -140,7 +79,8 @@ class Board {
                 board[i][j] = 0;
                 board[i][col] = value<<1;
                 isUpdated = true;
-                updateVector(value);
+                shifted = true;
+                occ--;
                 col++;
                 break;
               }
@@ -157,7 +97,8 @@ class Board {
 
         }while(j<board_size);
       }
-      if (!isUpdated) move = 1;
+
+      if (!shifted) move = 1;
       else move = -1;
       return isUpdated;
     }
@@ -167,7 +108,7 @@ class Board {
       int i, j;
       int col;
       int value, value_next;
-      bool isUpdated = false;
+      bool isUpdated = false, shifted = false;
 
       for(i = 0; i< board_size; i++){
 
@@ -193,7 +134,8 @@ class Board {
                 board[i][j] = 0;
                 board[i][col] = value<<1;
                 isUpdated = true;
-                updateVector(value);
+                shifted = true;
+                occ--;
                 col--;
                 break;
               }
@@ -201,7 +143,6 @@ class Board {
                 board[i][j] = 0;
                 board[i][--col] = value_next;
                 value = value_next;
-                //isUpdated = true;
               }
 
               j--;
@@ -211,8 +152,7 @@ class Board {
 
         }while(j>=0);
       }
-      //cout << "isupdated: " << isUpdated << '\n';
-      if (!isUpdated) move = 1;
+      if (!shifted) move = 1;
       else move = -1;
       return isUpdated;
     }
@@ -223,6 +163,7 @@ class Board {
         int line;
         int value, value_next;
         bool isUpdated = false;
+        bool shifted = false;
 
         // percorrer todas as colunas
         for(j = 0; j< board_size; j++){ //i conta a coluna
@@ -249,7 +190,8 @@ class Board {
                         board[i][j] = 0;
                         board[line][j] = value<<1;
                         isUpdated = true;
-                        updateVector(value);
+                        shifted = true;
+                        occ--;
                         line++;
                         break;
                     }
@@ -263,7 +205,7 @@ class Board {
             }
             }while(i<board_size); //enquanto não chega ao final da linha/coluna
         }
-        if (!isUpdated) move = 2;
+        if (!shifted) move = 2;
         else move = -1;
         return isUpdated;
     }
@@ -274,6 +216,7 @@ class Board {
         int line;
         int value, value_next;
         bool isUpdated = false;
+        bool shifted = false;
 
         // percorrer todas as colunas
         for(j = 0; j < board_size; j++){ //i conta a coluna
@@ -283,8 +226,6 @@ class Board {
             do{
             value = board[i][j];
             i--;
-
-            // cout << value << " ";
 
             if(value != 0){
 
@@ -302,7 +243,8 @@ class Board {
                     board[i][j] = 0;
                     board[line][j] = value<<1;
                     isUpdated = true;
-                    updateVector(value);
+                    shifted = true;
+                    occ--;
                     line--;
                     break;
                 }
@@ -316,27 +258,17 @@ class Board {
             }
             }while(i>=0); //enquanto não chega ao final da linha/coluna
         }
-        if (!isUpdated) move = 2; // there was no shift so there is no need to try it again later
+        if (!shifted) move = 2;
         else move = -1;
         return isUpdated;
     }
 
-    bool isPossible( vector<tuple<int, int>> vector_aux){
-
-        vector<tuple<int, int>>::iterator it;
-        int soma = 0;
-
-        for(it = vector_aux.begin(); it < vector_aux.end(); it++){
-
-            soma += get<0>(*it) * get<1>(*it);
-
-        }
-
-        return floor(log2(soma)) == log2(soma);
+    bool isPossible(){
+        return floor(log2(sum)) == log2(sum);
     }
 
     bool isCompleted () {
-        return vector_occ.size() == 1 && get<1>(*vector_occ.begin()) == 1;
+        return occ == 1;
     }
 };
 
@@ -353,15 +285,13 @@ void countMoves(Board &board, int counter, int& best, int previous_move){
     }
 
     // rejection condition
-    if (!(counter < best && counter < max_moves &&
-         !(best - counter < 2 &&
-         (board.vector_occ.size() > 1  || get<1>(*board.vector_occ.begin()) > 2)))) {
+    if (!(counter < best && counter < max_moves && !(best - counter < 2 && board.occ > 2))) {
         return;
     }
 
     counter++;
     //board.printBoard();
-    //cout << "prev_move: " << previous_move << '\n';
+    //if (previous_move!=-1) cout << "prev_move: " << previous_move << '\n';
     if (previous_move != 1) {
         aux = board;
         if (aux.shiftRight(previous_move)) {
@@ -383,7 +313,6 @@ void countMoves(Board &board, int counter, int& best, int previous_move){
             countMoves(aux, counter, best, previous_move);
         }
     }
-
     if (previous_move != 2) {
         aux = board;
         if (aux.shiftBottom(previous_move)){
@@ -395,7 +324,7 @@ void countMoves(Board &board, int counter, int& best, int previous_move){
 }
 
 void printMoves(Board board) {
-    if (board.isPossible(board.vector_occ)) {
+    if (board.isPossible()) {
 
         int best = max_moves +1;
         countMoves(board, 0, best, -1);
