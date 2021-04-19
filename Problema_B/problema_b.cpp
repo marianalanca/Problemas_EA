5b579a674efcd67418c1f449a8c8ac65249cc458
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2021-04-19 18:03:28 +0100

    60

--- Problema_B/problema_b.cpp
+++ Problema_B/problema_b.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 /*
@@ -15,8 +16,8 @@ using namespace std;
 
 */
 
-vector<vector<int>> T;
 int module = 1000000007;
+int n, h, H;
 
 // Statement formulas
 int mod_abs(int a, int mod) {
@@ -38,67 +39,74 @@ void print_matrix (vector<vector<int>> matrix) {
     cout << endl;
   }
 }
-
-int arc(int n, int h, int H){
-  // mudar para resizes!
-  int hi = 0, hi1 = 0;
-  T.resize(n + 1);
-  // são os primeiros 3 - alterar, eleminar 2 1ªs linhas
-  T[0].resize(H + 1, 0); // {1st base case}
-  T[1].resize(H + 1, 0); // {1st base case}
-  T[2].resize(H + 1, 0); // {1st base case}
-
-  for (int j=3; j < n+1; j++){ // {2nd base case}
-    T[j].resize(H + 1); // h+1?
-    T[j][0] = H+1;
+void print_vector (vector<int> matrix) {
+  for ( const auto &row : matrix )
+  {
+    cout << row << ' ';
+    cout << '\n';
   }
-
-  // mal
-  for (int i=3; i<n+1; i++) {
-    for (int j=1; j<H+1; j++) {
-      for (int k=1; k<h; k++) {
-        hi1 = hi + k;
-        if (hi1 > j) {
-          T[i][j] = T[i-1][j];
-        } else {
-          T[i][j] = max(T[i-1][j], 1 + T[i][j-hi1]);
-        }
-      }
-    }
-  }
-  //print_matrix(T);
-  return T[n][H];
+  cout << endl;
 }
 
-/*
-Function change(n, C)
-  for i = 0 to n do
-    T[i, 0] = 0
-  for j = 1 to C do
-    T[0, j] = ∞
-  for i = 1 to n do
-    for j = 1 to C do
-      if di > j then
-        T[i, j] = T[i − 1, j]
-      else
-        T[i, j] = min(T[i − 1, j], 1 + T[i, j − di])
-  return T[n, C]
+int sum_vector(vector<int> vec) {
+  int total = 0;
+  for (auto& v : vec)
+    total += v;
+  return total;
+}
+// direction -> 1 - up; 0 -> down
+vector <int> DP;
+int accepted;
+int arc(int ni, int hi, bool direction){
 
-*/
+  if (ni >= 3 && hi==h) { // se chegou ao final e está na posição certa -> conta
+   return 1;
+  }
+  if (ni == n && hi!=h) { // mudar!!! -> se chegou ao final e não está no chão! -> não conta
+    return 0;
+  }
+
+  for (int k=1; k < h; k++) {
+
+    if (direction) {
+      if (hi+k <=H){
+        //cout << "trying to add ascending " << hi+k << "\tH "<<H <<'\n';
+        DP[ni] += arc(ni+1, hi + k, true);
+        //cout << "var1: "<<var1 << '\n';
+      }
+    }
+
+    if (hi-k >= h) {
+      //cout << "trying to add descending " << hi-k <<'\n';
+      DP[ni] += arc(ni+1, hi - k, false);
+    }
+  }
+
+  return DP[n];
+}
 
 int main() {
     // t - number of input lines
     // h - Height of the Lego blocks
     // n - number of Lego blocks given
     // H - Height of ceiling
-    int h, t, n, H;
+    int t;
 
     cin >> t;
 
     for (int i =0; i<t;i++){
-        T.clear();
         cin >> n >> h >> H;
-        cout << mod_abs(arc(n, h, H), module) << '\n';
+
+        if ( n<3 || h >= H) {
+          cout << "0\n";
+          continue;
+        }
+        DP.clear();
+        DP.resize(n+1);
+        DP[0] = 0;
+        accepted = 0;
+        arc(1, h, true);
+        cout << mod_abs(sum_vector(DP), module) << '\n';
     }
 
     return 0;
