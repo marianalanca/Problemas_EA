1a3b050e942f3002ffbeb3f2615c6d54d822d82a
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2021-04-22 23:15:50 +0100

    Co-authored-by: MarianaLoreto30 <MarianaLoreto30@users.noreply.github.com>

--- Problema_B/problema_b.cpp
+++ Problema_B/problema_b.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cmath>
-#include <array>
 #include <algorithm>
 using namespace std;
 
@@ -10,9 +8,7 @@ int module = 1000000007;
 int n, h, H;
 vector<vector<int>> previous, curr;
 int total;
-vector<int> aux(2,0);
 
-// Statement formulas
 int mod_abs(int a, int mod) {
   return ((a % mod) + mod) % mod;
 }
@@ -21,48 +17,25 @@ int mod_add(int a, int b, int mod) {
   return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
 }
 
-
-int mod_sub(int a, int b, int mod) {
-  return mod_add(a, -b, mod);
-}
-
-int sum_line(vector<vector<int>> vector){
-  int sum = 0;
-  for (int i=2;i<(int)vector.size();i++){
-    sum= mod_add(sum, vector[i][1], module);
-  }
-  return sum;
-}
-
-void reserve_T() {
-  previous.resize(H-h+1, aux);
-  curr.resize(H-h+1, aux);
-
-  previous[0][0] = 1;
-}
-
 void arc(){
   for (int i=0;i<n-1;i++) {
-    for (int j=0;j<=H-h;j++) {
-      if (previous[j][0]!=0 || previous[j][1]!=0) {
-        for (int k=1;k<h;k++) {
-          // subida
-          if(j+k<=H-h && previous[j][0]!=0) {
-            curr[j+k][0]= mod_add(curr[j+k][0], previous[j][0], module);
-          }
-
-          // descida
-          if(j-k>=0){
-            curr[j-k][1] = mod_add(curr[j-k][1], previous[j][0], module);
-            curr[j-k][1] = mod_add(curr[j-k][1], previous[j][1], module);
-          }
+    for (int j=0;j<=H-h && !(j>n+1 && curr[j][0]==0);j++) {
+      for (int k=1;k<h;k++) {
+        // subida
+        if(j+k<=H-h && previous[j][0]!=0) {
+          curr[j+k][0]= mod_add(curr[j+k][0], previous[j][0], module);
         }
 
+        // descida
+        if(j-k>=0){
+          curr[j-k][1] = mod_add(curr[j-k][1], previous[j][0], module);
+          curr[j-k][1] = mod_add(curr[j-k][1], previous[j][1], module);
+        }
       }
     }
     total = mod_add(total, curr[0][1], module); // ignora logo a primeira linha e na segunda não pode ter
     previous = curr;
-    fill(curr.begin(), curr.end(), aux); // ????????????????
+    fill(curr.begin(), curr.end(), vector<int> (2,0));
   }
 }
 
@@ -71,7 +44,7 @@ int main() {
     // h - Height of the Lego blocks
     // n - number of Lego blocks given
     // H - Height of ceiling
-    int t;
+    int t, H_aux;
     cin >> t;
 
     for (int i =0; i<t;i++){
@@ -83,8 +56,22 @@ int main() {
         }
         previous.clear();
         curr.clear();
-        reserve_T();
-        // mudar modulo
+
+        if(n%2==0){
+            H_aux = h + (h-1)*(n/2 - 1);
+        }
+        else{
+            H_aux = h + (h-1)*((n+1)/2 - 1);
+        }
+
+        if(H > H_aux){
+            H = H_aux;
+        }
+
+        previous.resize(H-h+1, vector<int> (2,0));
+        curr.resize(H-h+1, vector<int> (2,0));
+
+        previous[0][0] = 1;
         arc();
         cout << total << '\n';
         total = 0;
