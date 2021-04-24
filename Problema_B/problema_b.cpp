3faccd5f6ebb9f902b53a074ebf80773922288f7
Mariana Luisa Lanca Miguel E Fernandes Marques <mlmarques@student.dei.uc.pt>
2021-04-24 22:11:45 +0100

    comentários

--- Problema_B/problema_b.cpp
+++ Problema_B/problema_b.cpp
@@ -21,37 +21,31 @@ int mod_sub(int a, int b, int mod) {
   return mod_add(a, -b, mod);
 }
 
-void print_vector (vector<vector<int>> vector) {
-  for (auto &row: vector) {
-    cout << "[" << row[0] << ", " << row[1]<< "]\n";
-  }
-  cout << "\n";
-}
-
 int calc(vector<vector<int>> &previous, vector<vector<int>> &curr, int i){
-  // (j-h<i && previous[i-1][0]!=0) 
+  // (j-h<i && previous[i-1][0]!=0)
   for (int j=0;j<=H-h && ((j-h<i) || (j-h>=i && previous[j-h][0]!=0));j++) {          // * O(H-h-1)
     // subida
     curr[j]= vector<int> (2,0); // esvaziar/
 
     // descida
-    if (j==0){
+    if (j==0){ // se estiver na linha mais abaixo calcula todos
       for (int k=1;k<h && j+k<=H-h && (previous[j+k][0]!=0 || previous[j+k][1]!=0) ;k++) {   // * O(h-1)
         curr[j][1] = mod_add(curr[j][1], previous[j+k][0], module);
         curr[j][1] = mod_add(curr[j][1], previous[j+k][1], module);
       }
-    } else if (j>0) {
+    } else if (j>0) { // se estiver mais acima, já foi calculado o mais abaixo e é possível reaproveitar
       // descida                                                                       // * O(1)
-      curr[j][1] = curr[j-1][1];
-      curr[j][1] = mod_sub(curr[j][1], previous[j][1], module);
+      curr[j][1] = curr[j-1][1];  // vai-se buscar o valor da descida do bloco que está exatamente abaixo
+      curr[j][1] = mod_sub(curr[j][1], previous[j][1], module); // subtraiem-se os valores do bloco da coluna anterior
       curr[j][1] = mod_sub(curr[j][1], previous[j][0], module);
 
       if (j+h-1<=H-h){
-        curr[j][1] = mod_add(curr[j][1], previous[j+h-1][0], module);
+        curr[j][1] = mod_add(curr[j][1], previous[j+h-1][0], module); // se possível, soma-se o módulo que está na altura j+h-1
         curr[j][1] = mod_add(curr[j][1], previous[j+h-1][1], module);
       }
 
       if (j>=i) {
+        // subida -> processo inverso ao da descida
         if (curr[j-1][0]==0) {  // o de baixo ainda não foi calculado
           for (int k=1;k<h && j-k>=0;k++) {                                             // * O(h-2)
             curr[j][0]= mod_add(curr[j][0], previous[j-k][0], module);
@@ -73,6 +67,7 @@ int calc(vector<vector<int>> &previous, vector<vector<int>> &curr, int i){
 
 void arc(){
   total = 0;
+  // permite alternar os vetores sem ser necessário fazer cópia
   for (int i=1;i<n;i++) {                                         // * O (n-2)
     if (i%2!=0) {
       total = mod_add(total, calc(vec1, vec2,i), module);
